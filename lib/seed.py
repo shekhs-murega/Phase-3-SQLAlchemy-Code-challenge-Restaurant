1fb5d081037286ab1334a39f29aaa2445ef2069c
shekinah <nyamburamary1212@gmail.com>
2023-12-09 15:13:02 +0300

    seeding data

---
+++ lib/seed.py
@@ -0,0 +1,59 @@
+from faker import Faker
+import random
+from models import engine, session, Restaurant, Customer, Review
+
+if __name__ == '__main__':
+    session.query(Restaurant).delete()
+    session.query(Customer).delete()
+    session.query(Review).delete()
+
+    fake = Faker()
+
+    restaurants = []
+    for j in range(50):
+        restaurant = Restaurant(
+            name=fake.unique.company(),
+            price=random.randint(1000, 50000)
+        )
+
+        session.add(restaurant)
+        restaurants.append(restaurant)
+        session.commit()
+
+    customers = []
+    for i in range(50):
+        customer = Customer(
+            first_name=fake.first_name(),
+            last_name=fake.last_name()
+        )
+
+        session.add(customer)
+        customers.append(customer)
+
+        session.commit()
+
+
+reviews = []
+star_ratings = [0, 1, 2, 3, 4, 5]
+
+for restaurant in restaurants:
+    for i in range(random.randint(1,50)):
+        customer = random.choice(customers)
+    
+
+        star_rating = random.choice(star_ratings)
+        print(f"Star Rating: {star_rating}")
+
+        review = Review(
+                star_rating=star_rating,
+                restaurant_id=restaurant.id,
+                customer_id=customer.id
+            )
+        reviews.append(review)
+
+# for review in reviews:
+    session.add(review)
+
+
+session.commit()
+session.close()
